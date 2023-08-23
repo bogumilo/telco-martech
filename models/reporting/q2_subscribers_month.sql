c46f56919403d29eac6e22f6153bb95d262fab32
Paul Bogumilo <operations@airpost.net>
2023-08-23 22:47:42 +0000

    feat: q2 answer

---
+++ models/reporting/q2_subscribers_month.sql
@@ -0,0 +1,37 @@
+with transformed as (
+    select * from {{ ref("dim_customer_details") }}
+),
+
+counter as (
+
+    select
+        j_month,
+    
+        COUNT(*) as subscribers_cnt
+    
+    from transformed
+
+    group by 1
+),
+
+min_max as (
+
+    select
+        j_month,
+
+        min(subscribers_cnt) over () as min_subs,
+        max(subscribers_cnt) over () as max_subs
+
+    from counter
+
+    -- group by 1
+)
+select
+    c.*,
+    case when c.subscribers_cnt = m.min_subs then 'Y' else 'N' end as the_worst_month,
+    case when c.subscribers_cnt = m.max_subs then 'Y' else 'N' end as the_best_month,
+    m.min_subs,
+    m.max_subs
+
+from counter c
+join min_max m on c.j_month = m.j_month
